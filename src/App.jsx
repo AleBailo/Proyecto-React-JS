c1ce13b503015c2d0657f9e6e9176bc19bce1769
Alejandro Bailo <ale_bailo@hotmail.com>
2025-12-16 11:00:22 -0300

    primer clase

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import button from './components/button/button'
+
+export const App = () => {
+  return (
+    <>
+      <h1>Hola, buen día!</h1>
+      <p>Bienvenido a mi aplicación React.</p>
+      <button/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const button = () => {
+  return (
+    <button>Hacé click aquí</button>
+  )
+}
+
+export default button
